0240591535ca0f3cb3c0e2d4cca065d7c8066c3b
cribibi <cricler_bianca_melania@yahoo.com>
2020-04-01 17:50:23 +0300

    "new commit"

--- src/euclid/Euclid.java
+++ src/euclid/Euclid.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Euclid {
     public static int cmmdc(int a, int b) {
-            while (a != b) {
-                if (a > b) {
-                    a = (a - b); //merge si cu scadere si cu impartire, impartire  mai rapid, vom lua modulo a%b
-                } else if (a < b) {
-                    b = (b - a);
-                }
-            }return a;
+        while (a != b) {
+            if (a > b) {
+                a = (a - b); //merge si cu scadere si cu impartire, impartire  mai rapid, vom lua modulo a%b
+            } else if (a < b) {
+                b = (b - a);
+            }
         }
+        return a;
+    }
 
-    public static int factorial(int n){
+    public static int factorial(int n) {
         if (n == 1) {
             return 1;
         } else {
@@ -23,21 +24,23 @@ public class Euclid {
 
     public static void main(String[] args) {
         Scanner tastatura = new Scanner(System.in);
-        System.out.println("dati a: ");
+        System.out.println("Type the first number, a: ");
         int a = tastatura.nextInt();
-        System.out.println("dati b: ");
+        System.out.println("Type the second number, b: ");
         int b = tastatura.nextInt();
 
-        if ((a!=0)&&(b!=0)){
-        System.out.println("cmmdc: " + cmmdc(a,b));}
-        else{
-            System.out.println("cel putin un nr este 0");
+        if ((a != 0) && (b != 0)) {
+            System.out.println("Cel mai mare divizor comun: " + cmmdc(a, b));
+        } else {
+            System.out.println("At least one of the numbers is 0");
         }
 
-        System.out.println("introduceti n: ");
+        System.out.println("Type n: ");
         int n = tastatura.nextInt();
-        System.out.printf(n+"!="+factorial(n));
-        }
+        System.out.println("------------------------");
+        System.out.printf(n + "!=" + factorial(n));
+
     }
+}
 
 
